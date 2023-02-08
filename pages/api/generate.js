75f3737a3070ff9c616194ac9e03627865c09717
UnnamedMistress <momchrysti@gmail.com>
2023-02-07 19:36:34 -0500

    Fully Functional Meal generator

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -16,6 +16,9 @@ export default async function (req, res) {
   }
 
   const text = req.body.text || '';
+  const meals = req.body.meals || 1;
+  const servings = req.body.servings || 1;
+  const diet = req.body.diet || " ";
   const words = text.trim().split(/\s+/).length;
 
   if (words < 5 || words > 250) {
@@ -31,9 +34,9 @@ export default async function (req, res) {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       // PROMPT GOES HERE// To Do: add your own prompt here
-      prompt: "Write a comprehensive, grammatically correct, and polished text based on the following prompt. Ensure that all spelling errors are corrected, and all information is clearly explained" + text + "\n\n",
+      prompt: "List" + meals + "meals that can be cooked with only these ingredients included in this prompt, use only 6 ingredients or less in each recipe, Show the recipe as well, Diet type " +diet + " or just use the standard american diet. include at least one breakfast meal and a at least one lunch meal and the rest are dinners. Each meal to be numbered and the recipe following it and a line space between meals, recipe instructions should be not numbered but alphabetical (a,b,c) and so on. include cook time. End each recipe with || to separate them. Ingredients: milk, eggs, butter, bread, salt, pepper, oil, sugar, cheese slice, ketchup, mustard" + text + "for number of servings" + servings +"\n\n",
       ///PROMPT ENDS HERE
-      max_tokens: 250,
+      max_tokens: 1000,
       temperature: 0,
     });
     res.status(200).json({ result: completion.data.choices[0].text });
--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,22 @@ import styles from "./index.module.css";
 
 export default function Home() {
   const [textInput, setTextInput] = useState("");
+const [mealCount, setMealCount] = useState(1);
+const [servingCount, setServingCount] = useState(1);
+const [dietInput, setDietInput] = useState(" ");
   const [result, setResult] = useState(" ");
+  const [loading, setLoading] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
+    setLoading(true);
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ text: textInput }),
+        body: JSON.stringify({ text: textInput, meals: mealCount, servings: servingCount }),
       });
 
       const data = await response.json();
@@ -24,39 +29,158 @@ export default function Home() {
 
       setResult(data.result);
       setTextInput("");
+      setLoading(false);
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
+      setLoading(false);
     }
   }
 
   return (
-    <div>
-      <Head>
-        <title>AI Editor</title>
-        <link rel="icon" href="/writingicon.png" />
-      </Head>
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          backgroundColor: "Coral",
+          padding: "1rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Head>
+          <title>Food At Home </title>
+        </Head>
+        <main
+          className={styles.main}
+          style={{
+            width: "100%",
+            display: "flex",
+            flexWrap: "wrap",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+            padding: "2%"
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "2%",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <h1 style={{ textAlign: "center", fontSize:"45px"}}> 🍴   Food At Home    🍴</h1>
+    <div style = {{textAlign: "center"}}>
+              <textarea
+              id = "textarea"
+                rows="5"
+                cols="40"
+                name="text"
+                placeholder="List the ingredients that you have. We'll assume you have staple items such as milk, eggs, and butter."
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}
+              /> <br></br>
+             </div>
+            
+            <div
+              id="form"
+              style={{ backgroundColor: "Coral", display: "flex", textAlign: "center", flexDirection: "column", alignItems: "center" }}
+            >
+              <form
+                onSubmit={onSubmit}
+                style={{ backgroundColor: "Coral", display: "flex", textAlign: "center", flexDirection: "column", alignItems: "center"}}
+              >
+                <div style={{ display: "flex", marginTop: "1rem" }}>
+                  <div style={{ padding: "2%" }}>
+                    <label htmlFor="meals">How many meals?</label>
+                    <input
+                      type="number"
+                      id="meals"
+                      name="meals"
+                      min="1"
+                      max="10"
+                      value={mealCount}
+                      onChange={(e) => setMealCount(e.target.value)}
+                    />
+                  </div>
+                 
+                </div>
+                <div style={{ textAlign: "center", padding: "2%" }}>
+                  <label htmlFor="diet">
+                    Diets or restrictions? (Or leave blank)
+                  </label>
+                  <br />
+                  <textarea
+                    id="textarea"
+                    rows="2"
+                    cols="15"
+                    name="diet"
+                    placeholder = "Enter diet restrictions, Low-Carb, Vegan, etc. or leave blank"
+                    value={dietInput}
+                    onChange={(e) => setDietInput(e.target.value)}
+                  />
+                  <br />
+             <div style = {{padding: "2%"}}>
+                    <label htmlFor="servings">
+                      How many are hungry? (1-20)
+                    </label>
+                    <input
+                      type="number"
+                      id="servings"
+                      name="servings"
+                      min="1"
+                      max="20"
+                      value={servingCount}
+                      onChange={(e) => setServingCount(e.target.value)}
+                    />
+                  </div>
+              </div> <input type="submit" value="Show Me What I Can Eat At Home" />
+              <br></br>
+              <input 
+                type="submit"
+                value="Reset"
+                onClick={() => {
+                  setTextInput("");
+                  setResult("");
+                }}
+              />
+            </form></div>
+          </div>
+        </main>
+      </div>
+      <div style={{ width: "100%", backgroundColor: "#03A9F4", padding: "1rem" }}>
+        {loading ? (
+          <div>
+            ... Thinking of delicious meals 🍴
+            <h2>I'm new and a little slow this may take a few mins</h2>
+          </div>
+        ) : (
+          <div className={styles.result}> <h1> Here is what you can make!</h1>
+            {result.split("\n").map((line, index) => (
+              <div key={index} style={{ marginLeft: "5%" }}>
+                {line.split(" || ").map((dish, index) => (
+                  <div key={index}>
+                    {dish}
+                    <br />
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+)}
+</div>
+</div>
 
-      <main className={styles.main}>
-        <img src="/writingicon.png" className={styles.icon} />
-        <h3>AutoCorrect on Steroids!</h3>
-        <p style={{fontFamily: "verdana"}}> Introducing a cutting-edge app that uses JavaScript, Node, React, and OpenAI API to provide the ultimate autocorrect solution. With this technology, I guarantee that you'll never have to worry about misspelled words or grammatical errors again. Just imagine sending an important email, and my app make all necessary text corrections. No more embarrassing typos, only flawless writing. Give it a try now and witness the difference for yourself. Use this sample text if you don't have any.</p>
-        <p>"Why due people say “break uh leg” wen u go awn staje? because eviry play has uh cast.Why ar their gates around cemetereis? because people ar dyin two jet inn."</p>
-        <form onSubmit={onSubmit}>
-          <textarea
-            rows="10"
-            cols="50"
-            name="text"
-            placeholder="Enter text (max 250 words)"
-            value={textInput}
-            onChange={(e) => setTextInput(e.target.value)}
-          />
-          <input type="submit" value="Edit text" />
-          <p>Give me a few seconds, I'm new and a little slow</p>
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
-  );
-}
+);
+        }
